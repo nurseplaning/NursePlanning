3489f69cf0502a87e761dc306d8e95586db29a71
hwillians <66992569+hwillians@users.noreply.github.com>
2021-06-20 21:35:51 +0200

    login works

--- WebNursePlanning/Controllers/HomeController.cs
+++ WebNursePlanning/Controllers/HomeController.cs
@@ -11,101 +11,117 @@ namespace WebNursePlanning.Controllers
 {
 	public class HomeController : Controller
 	{
-		private readonly ILogger<HomeController> _logger;
-		private readonly UserManager<Person> _userManager;
-		private readonly SignInManager<Person> _signInManager;
-		private readonly RoleManager<IdentityRole> _roleManager;
+		private readonly ILogger<HomeController> logger;
+		private readonly UserManager<Person> userManager;
+		private readonly SignInManager<Person> signInManager;
+		private readonly RoleManager<IdentityRole> roleManager;
 
 		public HomeController(ILogger<HomeController> logger,
 			UserManager<Person> userManager,
 			RoleManager<IdentityRole> roleManager,
 			SignInManager<Person> signInManager)
 		{
-			_logger = logger;
-			_userManager = userManager;
-			_roleManager = roleManager;
-			_signInManager = signInManager;
+			this.logger = logger;
+			this.userManager = userManager;
+			this.roleManager = roleManager;
+			this.signInManager = signInManager;
 		}
 
 		public async Task<IActionResult> IndexAsync()
 		{
-			var superNurse = new Nurse()
-			{
-				UserName = "eljefe",
-				FirstName = "El",
-				LastName = "Jefe",
-				BirthDay = new DateTime(1980, 10, 10),
-				Adress = "1 bis rue parallele 34000 Montpellier",
-				SiretNumber = "12345678987654",
-				Email = "eljefe@nurse.fr",
-				PhoneNumber = "0600000001",
-				EmailConfirmed = true,
-				TwoFactorEnabled = false,
-				PhoneNumberConfirmed = true,
-				LockoutEnabled = false,
-				AccessFailedCount = 0,
-			};
-			var password = "1MAY.2min";
-			IdentityResult chkSuperNurse = await _userManager.CreateAsync(superNurse, password);
+			#region Roles Generation
+
 			IdentityRole roleSuperAdmin = new IdentityRole()
 			{
 				Name = "ROLE_SUPER_ADMIN"
 			};
-			await _roleManager.CreateAsync(roleSuperAdmin);
-			await _userManager.AddToRoleAsync(superNurse, "ROLE_SUPER_ADMIN");
+			await roleManager.CreateAsync(roleSuperAdmin);
 
-			var nurse = new Nurse()
-			{
-				UserName = "Lolita",
-				FirstName = "Lola",
-				LastName = "Lopez",
-				BirthDay = new DateTime(1990, 12, 12),
-				Adress = "1 rue principale 34000 Montpellier",
-				SiretNumber = "12345678987654",
-				Email = "lolalopez@nurse.fr",
-				PhoneNumber = "0600000000",
-				PasswordHash = "mdpNurse",
-				EmailConfirmed = true,
-				TwoFactorEnabled = false,
-				PhoneNumberConfirmed = true,
-				LockoutEnabled = false,
-				AccessFailedCount = 0
-			};
-			var passwordNurse = "1MAY.2min";
-			IdentityResult chkNurse = await _userManager.CreateAsync(nurse, passwordNurse);
 			IdentityRole roleAdmin = new IdentityRole()
 			{
 				Name = "ROLE_ADMIN"
 			};
-			await _roleManager.CreateAsync(roleAdmin);
-			await _userManager.AddToRoleAsync(nurse, "ROLE_ADMIN");
+			await roleManager.CreateAsync(roleAdmin);
 
-			var patient = new Patient()
-			{
-				UserName = "elEnfermo",
-				FirstName = "Estoy",
-				LastName = "Malito",
-				BirthDay = new DateTime(1980, 12, 12),
-				Adress = "3, rue diagonale 34000 Montpellier",
-				SocialSecurityNumber = "1234567898765",
-				Email = "estoymalito@patient.fr",
-				PasswordHash = "mdpPatient",
-				PhoneNumber = "0600000002",
-				EmailConfirmed = true,
-				TwoFactorEnabled = false,
-				PhoneNumberConfirmed = true,
-				LockoutEnabled = true,
-				AccessFailedCount = 0
-			};
-
-			var passwordPatient = "1MAY.2min";
-			IdentityResult chkPatient = await _userManager.CreateAsync(patient, passwordPatient);
 			IdentityRole roleUser = new IdentityRole()
 			{
 				Name = "ROLE_USER"
 			};
-			await _roleManager.CreateAsync(roleUser);
-			await _userManager.AddToRoleAsync(superNurse, "ROLE_USER");
+			await roleManager.CreateAsync(roleUser);
+
+			#endregion Roles Generation
+
+			#region Default Users Generation
+
+			var users = userManager.GetUsersInRoleAsync("ROLE_SUPER_ADMIN");
+			if (users.Result.Count == 0)
+			{
+				var password = "Superpassword.0";
+
+				var superNurse = new Nurse()
+				{
+					UserName = "eljefe@nurse.fr",
+					FirstName = "El",
+					LastName = "Jefe",
+					BirthDay = new DateTime(1980, 10, 10),
+					Adress = "1 bis rue parallele 34000 Montpellier",
+					SiretNumber = "12345678987654",
+					Email = "eljefe@nurse.fr",
+					PhoneNumber = "0600000001",
+					EmailConfirmed = true,
+					TwoFactorEnabled = false,
+					PhoneNumberConfirmed = true,
+					LockoutEnabled = false,
+					AccessFailedCount = 0,
+				};
+				IdentityResult chkSuperNurse = await userManager.CreateAsync(superNurse, password);
+
+				await userManager.AddToRoleAsync(superNurse, "ROLE_SUPER_ADMIN");
+
+				var nurse = new Nurse()
+				{
+					UserName = "lolalopez@nurse.fr",
+					FirstName = "Lola",
+					LastName = "Lopez",
+					BirthDay = new DateTime(1990, 12, 12),
+					Adress = "1 rue principale 34000 Montpellier",
+					SiretNumber = "12345678987654",
+					Email = "lolalopez@nurse.fr",
+					PhoneNumber = "0600000000",
+					PasswordHash = "mdpNurse",
+					EmailConfirmed = true,
+					TwoFactorEnabled = false,
+					PhoneNumberConfirmed = true,
+					LockoutEnabled = false,
+					AccessFailedCount = 0
+				};
+
+				IdentityResult chkNurse = await userManager.CreateAsync(nurse, password);
+				await userManager.AddToRoleAsync(nurse, "ROLE_ADMIN");
+
+				var patient = new Patient()
+				{
+					UserName = "estoymalito@patient.fr",
+					FirstName = "Estoy",
+					LastName = "Malito",
+					BirthDay = new DateTime(1980, 12, 12),
+					Adress = "3, rue diagonale 34000 Montpellier",
+					SocialSecurityNumber = "1234567898765",
+					Email = "estoymalito@patient.fr",
+					PasswordHash = "mdpPatient",
+					PhoneNumber = "0600000002",
+					EmailConfirmed = true,
+					TwoFactorEnabled = false,
+					PhoneNumberConfirmed = true,
+					LockoutEnabled = true,
+					AccessFailedCount = 0
+				};
+
+				IdentityResult chkPatient = await userManager.CreateAsync(patient, password);
+				await userManager.AddToRoleAsync(superNurse, "ROLE_USER");
+			}
+
+			#endregion Default Users Generation
 
 			return View();
 		}
