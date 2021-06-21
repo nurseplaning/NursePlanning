7f451b9cf8692e704169b8b6c20e8814081b0f24
Helvin <helvin.willians@gmail.com>
2021-06-21 15:32:50 +0200

    rows en cols

--- WebNursePlanning/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WebNursePlanning/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -50,6 +50,10 @@ namespace WebNursePlanning.Areas.Identity.Pages.Account
             [EmailAddress]
             [Display(Name = "E-mail")]
             public string Email { get; set; }
+            [EmailAddress]
+            [Display(Name = "Confirmation E-mail")]
+            [Compare("Email", ErrorMessage = "L'e-mail et la confiramtion d'e-mail ne correspondent pas.")]
+            public string ConfirmEmail { get; set; }
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
