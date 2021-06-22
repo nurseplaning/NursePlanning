f448231071419bcd77dfb81f53f45389f5353644
MajidEly <elyamani.abdelmjid@gmail.com>
2021-06-22 15:40:20 +0200

    wpf v1 test

--- WpfNursePlanning/MainWindow.xaml.cs
+++ WpfNursePlanning/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +24,46 @@ namespace WpfNursePlanning
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<MyModel> list = new ObservableCollection<MyModel>();
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            EditNurse editNurse = new EditNurse();
+            editNurse.Show();
+            this.Close();
+
+        }
         public MainWindow()
         {
             InitializeComponent();
+            LoadData();
+            list.Add(new MyModel { Id = 123, LastName = "aaaa", FirstName = "aaaaaa" });
+            list.Add(new MyModel { Id = 456, LastName = "bbbbb", FirstName = "bbbb" });
+            list.Add(new MyModel { Id = 789, LastName = "ccccc", FirstName = "cccccc" });
+            this.dgr.ItemsSource = list;
         }
+
+
+        private void LoadData()
+        {
+
+
+        }
+
+        public class MyModel
+        {
+            public int Id { get; set; }
+            public string LastName { get; set; }
+            public string FirstName { get; set; }
+
+
+
+        }
+
+
+
+
     }
 }
+
+
