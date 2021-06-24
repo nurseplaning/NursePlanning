477d70bd269be3975edc20853422c9124be057c4
antoine leleu <antoine-41@live.fr>
2021-06-24 17:49:53 +0200

    Adding calendar in the create view

--- WebNursePlanning/Controllers/AppointmentsController.cs
+++ WebNursePlanning/Controllers/AppointmentsController.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using DomainModel;
 using Repository.Interfaces;
 using Microsoft.AspNetCore.Identity;
+using WebNursePlanning.Models;
 
 namespace WebNursePlanning.Controllers
 {
@@ -81,18 +82,24 @@ namespace WebNursePlanning.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Appointment appointment)
+        public async Task<IActionResult> Create(AppointmentViewModel appointment)
         {
             if (ModelState.IsValid)
             {
+                var a = new Appointment()
+                {
+                    Date = appointment.Date,
+                    AtHome = appointment.AtHome,
+                    NurseId = appointment.NurseId,
+                    PatientId = appointment.PatientId,
+                    Description = appointment.Description,
+                    StatusId = appointment.StatusId
+                };
                 //appointment.Id = Guid.NewGuid();
-                await _appointmentRepository.Create(appointment);
+                await _appointmentRepository.Create(a);
 
                 return RedirectToAction(nameof(Index));
             }
-            //ViewData["NurseId"] = new SelectList(_context.Nurses, "Id", "Id", appointment.NurseId);
-            //ViewData["PatientId"] = new SelectList(_context.Patients, "Id", "Id", appointment.PatientId);
-            //ViewData["StatusId"] = new SelectList(_context.Statuses, "Id", "Name", appointment.StatusId);
             return RedirectToAction("Index");
         }
 
--- WebNursePlanning/Models/AppointmentViewModel.cs
+++ WebNursePlanning/Models/AppointmentViewModel.cs
@@ -1,21 +1,24 @@
 ﻿using DomainModel;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebNursePlanning.Models
 {
-    public class PatientViewModel
+    public class AppointmentViewModel
     {
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
+        [DataType(DataType.Time)]
+        public DateTime Time { get; set; }
+        [Display(Name = "A domicile")]
         public bool AtHome { get; set; }
-        public Nurse Nurse { get; set; }
+        [Display(Name = "Infirmier(e)")]
         public string NurseId { get; set; }
-        public Patient Patient { get; set; }
+        [Display(Name = "Patient(e)")]
         public string PatientId { get; set; }
-        public Status Status { get; set; }
         public string Description { get; set; }
+        [Display(Name = "Status")]
         public Guid StatusId { get; set; }
         public ICollection<Message> Messages { get; set; }
     }
