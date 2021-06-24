4a42987d03ec55aa0583ab1136b884fd6ffd1bc2
GouyonAntoine <gouyon.antoine@gmail.com>
2021-06-24 14:34:41 +0200

    Update

--- DomainModel/Appointment.cs
+++ DomainModel/Appointment.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DomainModel
 {
     public class Appointment
     {
         public Guid Id { get; set; }
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
         public string Description { get; set; }
         public bool AtHome { get; set; }
         public Nurse Nurse { get; set; }
+        [Display(Name = "Infirmier(e)")]
         public string NurseId { get; set; }
         public Patient Patient { get; set; }
+        [Display(Name = "Patent(e)")]
         public string PatientId { get; set; }
         public Status Status { get; set; }
         public Guid StatusId { get; set; }
