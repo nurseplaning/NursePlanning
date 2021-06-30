8d6420a16db5723c256ce6f96e461cdb00c4f45b
Formation <Formation@DESKTOP-8J8EC0K>
2021-06-30 15:17:46 +0200

    ajout de la fonction update pour nurse

--- WpfNursePlanning/MainWindow.xaml.cs
+++ WpfNursePlanning/MainWindow.xaml.cs
@@ -53,6 +53,20 @@ namespace WpfNursePlanning
                         BirthDay = item.BirthDay,
                         Adress = item.Adress,
                         SiretNumber = item.SiretNumber,
+                        IsActive = item.IsActive,
+                        UseEmailrName = item.UseEmailrName,
+                        UserName = item.UserName,
+                        PasswordHash = item.PasswordHash,
+                        SecurityStamp = item.SecurityStamp,
+                        ConcurrencyStamp = item.ConcurrencyStamp,
+                        NormalizedUserName = item.NormalizedUserName,
+                        Email = item.Email,
+                        NormalizedEmail = item.NormalizedEmail,
+                        PhoneNumber = item.PhoneNumber,
+                        LockoutEnabled = item.LockoutEnabled,
+                        PhoneNumberConfirmed = item.PhoneNumberConfirmed,
+                        TwoFactorEnabled = item.TwoFactorEnabled,
+                        EmailConfirmed = item.TwoFactorEnabled,
                     });
             }
             dgr.ItemsSource = list;
@@ -73,7 +87,22 @@ namespace WpfNursePlanning
             dpBithDay.SelectedDate = nurse.BirthDay;
             txtAdress.Text = nurse.Adress;
             txtSiretNumber.Text = nurse.SiretNumber;
+            txtPhoneNumber.Text = nurse.PhoneNumber;
+            txtUseEmailrName.Text = nurse.UseEmailrName;
+            txtUserName.Text = nurse.UserName;
+            txtPasswordHash.Text = nurse.PasswordHash;
+            txtSecurityStamp.Text = nurse.SecurityStamp;
+            txtConcurrencyStamp.Text = nurse.ConcurrencyStamp;
+            txtNormalizedUserName.Text = nurse.NormalizedUserName;
+            txtEmail.Text = nurse.Email;
+            txtNormalizedEmail.Text = nurse.NormalizedEmail;
+            txtLockoutEnabled.Text = "true";
+            txtPhoneNumberConfirmed.Text = "true";
+            txtTwoFactorEnabled.Text = "true";
+            txtEmailConfirmed.Text = "true";
+            EnableDesable(true);
         }
+      
 
         private void btnAppointement(object sender, RoutedEventArgs e)
         {
@@ -101,6 +130,58 @@ namespace WpfNursePlanning
             this.Close();
         }
 
+
+        void btnActiveNurse(object sender, RoutedEventArgs e)
+        {
+            Nurse nurse = dgr.SelectedItem as Nurse;
+
+            nurse.IsActive = (bool)chkIsActive.IsChecked;
+            nurse.LastName = txtLastName.Text;
+            nurse.FirstName = txtFirstName.Text;
+            nurse.BirthDay = (DateTime)dpBithDay.SelectedDate;
+            nurse.Adress = txtAdress.Text;
+            nurse.SiretNumber= txtSiretNumber.Text;
+            nurse.LockoutEnabled = txtLockoutEnabled.Text == "true";
+            nurse.PhoneNumberConfirmed = txtPhoneNumberConfirmed.Text == "true";
+            nurse.TwoFactorEnabled = txtTwoFactorEnabled.Text == "true";
+            nurse.EmailConfirmed = txtEmailConfirmed.Text == "true";
+            nurse.AccessFailedCount = 0;
+            nurse.PhoneNumber = txtPhoneNumber.Text;
+            nurse.UseEmailrName = txtUseEmailrName.Text;
+            nurse.UserName = txtUserName.Text;
+            nurse.PasswordHash = txtPasswordHash.Text;
+            nurse.SecurityStamp = txtSecurityStamp.Text;
+            nurse.ConcurrencyStamp = txtConcurrencyStamp.Text;
+            nurse.NormalizedUserName = txtNormalizedUserName.Text;
+            nurse.Email = txtEmail.Text;
+            nurse.NormalizedEmail = txtNormalizedEmail.Text;
+
+            this.UpdateNurse(nurse);
+        }
+
+        private async void UpdateNurse(Nurse nurse)
+        {
+            await client.PutAsJsonAsync(API_URL + "/" + nurse.Id, nurse);
+            MessageBox.Show("l'infirmier(e) " + nurse.LastName + " est modifié(e)");
+            EnableDesable(false);
+            dgr.Items.Refresh();
+
+        }
+
+        public void EnableDesable(bool i)
+        {
+            chkIsActive.IsEnabled = i;
+            txtLastName.IsEnabled = i;
+            txtFirstName.IsEnabled = i;
+            dpBithDay.IsEnabled = i;
+            txtAdress.IsEnabled = i;
+            txtSiretNumber.IsEnabled = i;
+            txtPhoneNumber.IsEnabled = i;
+            txtUseEmailrName.IsEnabled = i;
+            txtUserName.IsEnabled = i;
+            txtEmail.IsEnabled = i;
+            btnModifNurse.IsEnabled = i;
+        }
 
     }
 }
