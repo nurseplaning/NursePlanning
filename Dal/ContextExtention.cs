e64f07c734989a318f7d23a74755302963d597ea
Antoine L <antoine.leleu@meritis.fr>
2023-01-18 12:44:48 +0100

    Nettoyage de fichiers et ajout de catch si la bdd n'existe pas

--- Dal/ContextExtention.cs
+++ Dal/ContextExtention.cs
@@ -1,29 +1,37 @@
 ﻿using DomainModel;
+using System;
 using System.Linq;
 
 namespace Dal
 {
-	public static class ContextExtention
-	{
-		public static void Initialize(this ApplicationDbContext context, bool dropAlways = false)
-		{
-			if (dropAlways)
-				context.Database.EnsureDeleted();
+    public static class ContextExtention
+    {
+        public static void Initialize(this ApplicationDbContext context, bool dropAlways = false)
+        {
+            if (dropAlways)
+                context.Database.EnsureDeleted();
+            try
+            {
+                context.Database.EnsureCreated();
 
-			context.Database.EnsureCreated();
+            }
+            catch (Exception ex) 
+            {
+                Console.Error.WriteLine(ex.ToString()); 
+            }
 
-			if (context.Statuses.Any())
-				return;
+            if (context.Statuses.Any())
+                return;
 
-			var status = new Status[] {
-				new Status { Name = "En attente" },
-				new Status { Name = "Validé" },
-				new Status { Name = "Annulé" },
-				new Status { Name = "Rejeté" },
-				new Status { Name = "Fermé" } };
+            var status = new Status[] {
+                new Status { Name = "En attente" },
+                new Status { Name = "Validé" },
+                new Status { Name = "Annulé" },
+                new Status { Name = "Rejeté" },
+                new Status { Name = "Fermé" } };
 
-			context.Statuses.AddRange(status);
-			context.SaveChanges();
-		}
-	}
+            context.Statuses.AddRange(status);
+            context.SaveChanges();
+        }
+    }
 }
