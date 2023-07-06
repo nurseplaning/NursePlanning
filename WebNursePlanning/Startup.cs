0bca183111c490cfa5b10db9b51dcfb3e1f7cc67
Antoine <antoine-41@live.fr>
2023-07-06 14:48:49 +0200

    Docker containers working
    App + Sql !

--- WebNursePlanning/Startup.cs
+++ WebNursePlanning/Startup.cs
@@ -37,7 +37,7 @@ namespace WebNursePlanning
 
 			services.AddDbContext<ApplicationDbContext>(options =>
 					options.UseSqlServer(
-						Configuration.GetConnectionString("DockerConnection")));
+						Configuration.GetConnectionString("DockerConnectionProd")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
 			services.AddIdentity<Person, IdentityRole>()
